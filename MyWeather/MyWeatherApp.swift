70542a62b73967c9996fc4e5960cd54b1452effb
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-20 12:27:04 +0200

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Vladyslav Dikhtiaruk on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
