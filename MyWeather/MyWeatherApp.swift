8838b04cdb54379d258ef2d745246081dde46573
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-25 16:33:26 +0200

    Added initialiser for MyWeatherApp

--- MyWeather/MyWeatherApp.swift
+++ MyWeather/MyWeatherApp.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 import SwiftData
 
+
 @main
 struct MyWeatherApp: App {
+    let container: ModelContainer
+    let networkManager: NetworkServiceManager
+    let storageManager: StorageServiceManager
+    
+
     var body: some Scene {
         WindowGroup {
-            HomeCitiesView()
-                .modelContainer(for: CityModel.self)
+            HomeCitiesView(networkManager: networkManager, storageManager: storageManager)
+                .modelContainer(container)
+
         }
     }
+    init() {
+            do {
+                container = try ModelContainer(for: CityModel.self)
+                networkManager = NetworkServiceManager()
+                storageManager = StorageServiceManager(modelContext: container.mainContext)
+            } catch {
+                fatalError("Failed to create ModelContainer for CityModel. \(error.localizedDescription)")
+            }
+        }
 }
