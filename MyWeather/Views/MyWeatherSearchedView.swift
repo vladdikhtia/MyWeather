a975b4291a4bb2e1c6e58f9a6f0a7ca15225e0a6
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-26 16:46:42 +0200

    Implemented MyWeatherSearchView

--- MyWeather/Views/MyWeatherSearchedView.swift
+++ MyWeather/Views/MyWeatherSearchedView.swift
@@ -4,15 +4,24 @@
 //
 //  Created by Vladyslav Dikhtiaruk on 25/08/2024.
 //
-//
-//import SwiftUI
-//
-//struct MyWeatherSearchedView: View {
-//    var body: some View {
-//        MyWeatherMainView(viewModel: <#T##WeatherViewModel#>, navigationPath: <#T##Binding<NavigationPath>#>, lat: <#T##Binding<Double?>#>, lon: <#T##Binding<Double?>#>, isPresented: <#T##Binding<Bool>#>)
-//    }
-//}
-//
-//#Preview {
-//    MyWeatherSearchedView()
-//}
+
+import SwiftUI
+
+struct MyWeatherSearchedView: View {
+    @ObservedObject var viewModel: WeatherViewModel
+    var latitude: Double
+    var longitude: Double
+    @Binding var navigationPath: NavigationPath
+    
+    var body: some View {
+        WeatherView(viewModel: viewModel)
+            .onAppear{
+                viewModel.fetchWeather(latitude: latitude, longitude: longitude)
+            }
+    }
+}
+
+
+#Preview {
+    MyWeatherSearchedView(viewModel: WeatherViewModel(networkManager: MockNetworkManager(), storageManager: MockStorageManager()), latitude: 50, longitude: 30, navigationPath: .constant(NavigationPath()))
+}
